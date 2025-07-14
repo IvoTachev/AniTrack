5e39cc1d056c39ded7c21e51e62c1536065cf6a3
IvoTachev <ivo.tachev95@gmail.com>
2025-07-14 16:53:00 +0300

    Add project files.

---
+++ AniTrack.Data/AniTrackDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace AniTrack.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class AniTrackDbContext : IdentityDbContext
+    {
+        public AniTrackDbContext(DbContextOptions<AniTrackDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
