710f4c0a98701c07f1b9a4a8ffc3b7e93897633b
IvoTachev <ivo.tachev95@gmail.com>
2025-07-28 12:19:40 +0300

    Fixed some style issues in the Login page. Added Help section and added About View in it.

---
+++ AniTrack/Controllers/HelpController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniTrack.Web.Controllers
+{
+    public class HelpController : BaseController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> About()
+        {
+            return this.View();
+        }
+    }
+}
